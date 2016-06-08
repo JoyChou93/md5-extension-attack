a4b60a0088aaa6b9905a79e549dcae0662a14153
JoyChou93 <viarus@qq.com>
2016-06-08 13:13:53 +0800

    md5pad

--- md5pad.py
+++ md5pad.py
@@ -1,29 +1,32 @@
-#coding:utf-8
+# coding:utf-8
 
-import md5py,sys
+import md5py
+import sys
 from urllib import unquote
 import hashlib
 import struct
 import urllib
 
+
 def payload(length, str_append):
     pad = ''
-    n0 = ((56 - (length + 1)%64) % 64)
+    n0 = ((56 - (length + 1) % 64) % 64)
     pad += '\x80' 
     pad += '\x00'*n0 + struct.pack('Q', length*8)
-    length += 1 + n0 + 8
 
     return pad + str_append
+
 
 def hashmd5(str):
     return hashlib.md5(str).hexdigest()
 
+
 def check_extension_attack():
-    for i in range(1, 33):
+    for i in range(1, 65):
         s = "A" * i
         mm = md5py.md5()
         assert hashlib.md5(s).hexdigest() == mm.my_md5(s)
-
+        print mm.my_md5(s)
     for i in range(1, 100):
         for j in range(1, 10):
             s = 'A' * i
@@ -32,11 +35,11 @@ def check_extension_attack():
             msg = salt + s + payload(len(salt+s), 'joychou')
             assert hashmd5(msg) == mm.extension_attack(hashmd5(salt+s), 'joychou', len(salt+s))
 
-#check if md5 extension attack is correct 
-check_extension_attack()
+# check if md5 extension attack is correct
+# check_extension_attack()
 
-if len(sys.argv)<3:
-    print "Usage: ",sys.argv[0]," <md5string> <string_to_append> [length of plaintext of md5string]"
+if len(sys.argv) < 3:
+    print "Usage: ", sys.argv[0], " <md5string> <string_to_append> [length of plaintext of md5string]"
     sys.exit()
 
 
@@ -44,10 +47,9 @@ hash_origin = sys.argv[1]
 str_append = sys.argv[2]
 lenth = int(sys.argv[3])
 
-
 m = md5py.md5()
-str_payload = payload(lenth, str_append)
 
+str_payload = payload(lenth, str_append)
 print "Payload: ", repr(str_payload)
 print "Payload urlencode:", urllib.quote_plus(str_payload)
 print "md5:", m.extension_attack(hash_origin, str_append, lenth)
--- md5py.py
+++ md5py.py
@@ -1,23 +1,29 @@
-#coding:utf-8
+# coding:utf-8
 
 import struct
 import hashlib
 import binascii
 
+
 def F(x, y, z):
     return (x & y) | ((~x) & z)
+
 
 def G(x, y, z):
     return (x & z) | (y & (~z))
 
+
 def H(x, y, z):
     return x ^ y ^ z
+
 
 def I(x, y, z):
     return y ^ (x | (~z))
 
+
 def _rotateLeft( x, n):
     return (x << n) | (x >> (32-n))
+
 
 def XX(func, a, b, c, d, x, s, ac):
     res = 0L
@@ -34,7 +40,7 @@ def XX(func, a, b, c, d, x, s, ac):
 
 class md5():
     def __init__(self):
-        self.A, self.B, self.C, self.D =  (0x67452301L,0xefcdab89L,0x98badcfeL,0x10325476L)
+        self.A, self.B, self.C, self.D = (0x67452301L, 0xefcdab89L, 0x98badcfeL, 0x10325476L)
 
     def md5_compress(self, buf):
         if len(buf) != 64:
@@ -45,16 +51,16 @@ class md5():
         # Round 1.
         S11, S12, S13, S14 = 7, 12, 17, 22
 
-        a = XX(F, a, b, c, d, inp[ 0], S11, 0xD76AA478L) # 1
-        d = XX(F, d, a, b, c, inp[ 1], S12, 0xE8C7B756L) # 2
-        c = XX(F, c, d, a, b, inp[ 2], S13, 0x242070DBL) # 3
-        b = XX(F, b, c, d, a, inp[ 3], S14, 0xC1BDCEEEL) # 4
-        a = XX(F, a, b, c, d, inp[ 4], S11, 0xF57C0FAFL) # 5
-        d = XX(F, d, a, b, c, inp[ 5], S12, 0x4787C62AL) # 6
-        c = XX(F, c, d, a, b, inp[ 6], S13, 0xA8304613L) # 7
-        b = XX(F, b, c, d, a, inp[ 7], S14, 0xFD469501L) # 8
-        a = XX(F, a, b, c, d, inp[ 8], S11, 0x698098D8L) # 9
-        d = XX(F, d, a, b, c, inp[ 9], S12, 0x8B44F7AFL) # 10
+        a = XX(F, a, b, c, d, inp[0], S11, 0xD76AA478L) # 1
+        d = XX(F, d, a, b, c, inp[1], S12, 0xE8C7B756L) # 2
+        c = XX(F, c, d, a, b, inp[2], S13, 0x242070DBL) # 3
+        b = XX(F, b, c, d, a, inp[3], S14, 0xC1BDCEEEL) # 4
+        a = XX(F, a, b, c, d, inp[4], S11, 0xF57C0FAFL) # 5
+        d = XX(F, d, a, b, c, inp[5], S12, 0x4787C62AL) # 6
+        c = XX(F, c, d, a, b, inp[6], S13, 0xA8304613L) # 7
+        b = XX(F, b, c, d, a, inp[7], S14, 0xFD469501L) # 8
+        a = XX(F, a, b, c, d, inp[8], S11, 0x698098D8L) # 9
+        d = XX(F, d, a, b, c, inp[9], S12, 0x8B44F7AFL) # 10
         c = XX(F, c, d, a, b, inp[10], S13, 0xFFFF5BB1L) # 11
         b = XX(F, b, c, d, a, inp[11], S14, 0x895CD7BEL) # 12
         a = XX(F, a, b, c, d, inp[12], S11, 0x6B901122L) # 13
@@ -65,75 +71,75 @@ class md5():
         # Round 2.
         S21, S22, S23, S24 = 5, 9, 14, 20
 
-        a = XX(G, a, b, c, d, inp[ 1], S21, 0xF61E2562L) # 17
-        d = XX(G, d, a, b, c, inp[ 6], S22, 0xC040B340L) # 18
+        a = XX(G, a, b, c, d, inp[1], S21, 0xF61E2562L) # 17
+        d = XX(G, d, a, b, c, inp[6], S22, 0xC040B340L) # 18
         c = XX(G, c, d, a, b, inp[11], S23, 0x265E5A51L) # 19
-        b = XX(G, b, c, d, a, inp[ 0], S24, 0xE9B6C7AAL) # 20
-        a = XX(G, a, b, c, d, inp[ 5], S21, 0xD62F105DL) # 21
+        b = XX(G, b, c, d, a, inp[0], S24, 0xE9B6C7AAL) # 20
+        a = XX(G, a, b, c, d, inp[5], S21, 0xD62F105DL) # 21
         d = XX(G, d, a, b, c, inp[10], S22, 0x02441453L) # 22
         c = XX(G, c, d, a, b, inp[15], S23, 0xD8A1E681L) # 23
-        b = XX(G, b, c, d, a, inp[ 4], S24, 0xE7D3FBC8L) # 24
-        a = XX(G, a, b, c, d, inp[ 9], S21, 0x21E1CDE6L) # 25
+        b = XX(G, b, c, d, a, inp[4], S24, 0xE7D3FBC8L) # 24
+        a = XX(G, a, b, c, d, inp[9], S21, 0x21E1CDE6L) # 25
         d = XX(G, d, a, b, c, inp[14], S22, 0xC33707D6L) # 26
-        c = XX(G, c, d, a, b, inp[ 3], S23, 0xF4D50D87L) # 27
-        b = XX(G, b, c, d, a, inp[ 8], S24, 0x455A14EDL) # 28
+        c = XX(G, c, d, a, b, inp[3], S23, 0xF4D50D87L) # 27
+        b = XX(G, b, c, d, a, inp[8], S24, 0x455A14EDL) # 28
         a = XX(G, a, b, c, d, inp[13], S21, 0xA9E3E905L) # 29
-        d = XX(G, d, a, b, c, inp[ 2], S22, 0xFCEFA3F8L) # 30
-        c = XX(G, c, d, a, b, inp[ 7], S23, 0x676F02D9L) # 31
+        d = XX(G, d, a, b, c, inp[2], S22, 0xFCEFA3F8L) # 30
+        c = XX(G, c, d, a, b, inp[7], S23, 0x676F02D9L) # 31
         b = XX(G, b, c, d, a, inp[12], S24, 0x8D2A4C8AL) # 32
 
         # Round 3.
         S31, S32, S33, S34 = 4, 11, 16, 23
 
-        a = XX(H, a, b, c, d, inp[ 5], S31, 0xFFFA3942L) # 33
-        d = XX(H, d, a, b, c, inp[ 8], S32, 0x8771F681L) # 34
+        a = XX(H, a, b, c, d, inp[5], S31, 0xFFFA3942L) # 33
+        d = XX(H, d, a, b, c, inp[8], S32, 0x8771F681L) # 34
         c = XX(H, c, d, a, b, inp[11], S33, 0x6D9D6122L) # 35
         b = XX(H, b, c, d, a, inp[14], S34, 0xFDE5380CL) # 36
-        a = XX(H, a, b, c, d, inp[ 1], S31, 0xA4BEEA44L) # 37
-        d = XX(H, d, a, b, c, inp[ 4], S32, 0x4BDECFA9L) # 38
-        c = XX(H, c, d, a, b, inp[ 7], S33, 0xF6BB4B60L) # 39
+        a = XX(H, a, b, c, d, inp[1], S31, 0xA4BEEA44L) # 37
+        d = XX(H, d, a, b, c, inp[4], S32, 0x4BDECFA9L) # 38
+        c = XX(H, c, d, a, b, inp[7], S33, 0xF6BB4B60L) # 39
         b = XX(H, b, c, d, a, inp[10], S34, 0xBEBFBC70L) # 40
         a = XX(H, a, b, c, d, inp[13], S31, 0x289B7EC6L) # 41
-        d = XX(H, d, a, b, c, inp[ 0], S32, 0xEAA127FAL) # 42
-        c = XX(H, c, d, a, b, inp[ 3], S33, 0xD4EF3085L) # 43
-        b = XX(H, b, c, d, a, inp[ 6], S34, 0x04881D05L) # 44
-        a = XX(H, a, b, c, d, inp[ 9], S31, 0xD9D4D039L) # 45
+        d = XX(H, d, a, b, c, inp[0], S32, 0xEAA127FAL) # 42
+        c = XX(H, c, d, a, b, inp[3], S33, 0xD4EF3085L) # 43
+        b = XX(H, b, c, d, a, inp[6], S34, 0x04881D05L) # 44
+        a = XX(H, a, b, c, d, inp[9], S31, 0xD9D4D039L) # 45
         d = XX(H, d, a, b, c, inp[12], S32, 0xE6DB99E5L) # 46
         c = XX(H, c, d, a, b, inp[15], S33, 0x1FA27CF8L) # 47
-        b = XX(H, b, c, d, a, inp[ 2], S34, 0xC4AC5665L) # 48
+        b = XX(H, b, c, d, a, inp[2], S34, 0xC4AC5665L) # 48
 
         # Round 4.
         S41, S42, S43, S44 = 6, 10, 15, 21
 
-        a = XX(I, a, b, c, d, inp[ 0], S41, 0xF4292244L) # 49
-        d = XX(I, d, a, b, c, inp[ 7], S42, 0x432AFF97L) # 50
+        a = XX(I, a, b, c, d, inp[0], S41, 0xF4292244L) # 49
+        d = XX(I, d, a, b, c, inp[7], S42, 0x432AFF97L) # 50
         c = XX(I, c, d, a, b, inp[14], S43, 0xAB9423A7L) # 51
-        b = XX(I, b, c, d, a, inp[ 5], S44, 0xFC93A039L) # 52
+        b = XX(I, b, c, d, a, inp[5], S44, 0xFC93A039L) # 52
         a = XX(I, a, b, c, d, inp[12], S41, 0x655B59C3L) # 53
-        d = XX(I, d, a, b, c, inp[ 3], S42, 0x8F0CCC92L) # 54
+        d = XX(I, d, a, b, c, inp[3], S42, 0x8F0CCC92L) # 54
         c = XX(I, c, d, a, b, inp[10], S43, 0xFFEFF47DL) # 55
-        b = XX(I, b, c, d, a, inp[ 1], S44, 0x85845DD1L) # 56
-        a = XX(I, a, b, c, d, inp[ 8], S41, 0x6FA87E4FL) # 57
+        b = XX(I, b, c, d, a, inp[1], S44, 0x85845DD1L) # 56
+        a = XX(I, a, b, c, d, inp[8], S41, 0x6FA87E4FL) # 57
         d = XX(I, d, a, b, c, inp[15], S42, 0xFE2CE6E0L) # 58
-        c = XX(I, c, d, a, b, inp[ 6], S43, 0xA3014314L) # 59
+        c = XX(I, c, d, a, b, inp[6], S43, 0xA3014314L) # 59
         b = XX(I, b, c, d, a, inp[13], S44, 0x4E0811A1L) # 60
-        a = XX(I, a, b, c, d, inp[ 4], S41, 0xF7537E82L) # 61
+        a = XX(I, a, b, c, d, inp[4], S41, 0xF7537E82L) # 61
         d = XX(I, d, a, b, c, inp[11], S42, 0xBD3AF235L) # 62
-        c = XX(I, c, d, a, b, inp[ 2], S43, 0x2AD7D2BBL) # 63
-        b = XX(I, b, c, d, a, inp[ 9], S44, 0xEB86D391L) # 64
+        c = XX(I, c, d, a, b, inp[2], S43, 0x2AD7D2BBL) # 63
+        b = XX(I, b, c, d, a, inp[9], S44, 0xEB86D391L) # 64
 
         self.A = (self.A + a) & 0xffffffffL
         self.B = (self.B + b) & 0xffffffffL
         self.C = (self.C + c) & 0xffffffffL
         self.D = (self.D + d) & 0xffffffffL
-        #print 'A=%d\nB=%d\nC=%d\nD=%d\n'% (self.A, self.B, self.C, self.D)
+        # print 'A=%s\nB=%s\nC=%s\nD=%s\n' % (hex(self.A), hex(self.B), hex(self.C), hex(self.D))
 
     def padding(self, n, sz=None):
         if sz is None:
             sz = n
         pre = 64-8
         sz = struct.pack("Q",sz*8)
-        pad = chr(0b10000000) #0x80
+        pad = '\x80'
         n += 1
         if n % 64 <= pre:
             pad += '\0' * (pre - n % 64)
@@ -163,18 +169,19 @@ class md5():
         self.md5_iter(padding_msg)
         return self.hexdigest()
 
-    #通过md5值，逆向算出最后一轮的magic number
+    # 通过md5值，逆向算出最后一轮的magic number
     def compute_magic_number(self, md5str):
-        self.A = struct.unpack("I", md5str[0:8].decode('hex')) [0]
-        self.B = struct.unpack("I", md5str[8:16].decode('hex')) [0]
-        self.C = struct.unpack("I", md5str[16:24].decode('hex')) [0]
-        self.D = struct.unpack("I", md5str[24:32].decode('hex')) [0]
-        #print 'A=%d\nB=%d\nC=%d\nD=%d\n'% (self.A, self.B, self.C, self.D)
+        self.A = struct.unpack("I", md5str[0:8].decode('hex'))[0]
+        self.B = struct.unpack("I", md5str[8:16].decode('hex'))[0]
+        self.C = struct.unpack("I", md5str[16:24].decode('hex'))[0]
+        self.D = struct.unpack("I", md5str[24:32].decode('hex'))[0]
+        # print 'A=%s\nB=%s\nC=%s\nD=%s\n' % (hex(self.A), hex(self.B), hex(self.C), hex(self.D))
 
     def extension_attack(self, md5str, str_append, lenth):
         self.compute_magic_number(md5str)
         p = self.padding(lenth)
         padding_msg = self.padding( len(str_append), lenth + len(p) + len(str_append) )
-        s2 = self.md5_iter(str_append + padding_msg)
+        self.md5_iter(str_append + padding_msg)
         return self.hexdigest()
-                            
+
+
